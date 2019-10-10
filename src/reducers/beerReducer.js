d65addb42c3d8f0749347f15ddbf5dafe1b8a125
gelinas <jlgelinas@gmail.com>
2019-10-10 15:55:25 -0400

    frame styling complete

---
+++ src/reducers/beerReducer.js
@@ -0,0 +1,14 @@
+import {  } from '../actions'
+
+const initialState = {
+    test: 'test'
+}
+
+export const beerReducer = (state = initialState, action) => {
+    switch(action.type){
+        //case EXAMPLE:
+        //case EXAMPLE:
+        default:
+            return state;
+    }
+}
